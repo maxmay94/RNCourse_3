4098c675c4285d150cdc253869a8394a2c64549b
maxmay94 <maxmay94@gmail.com>
2023-01-18 14:25:52 -0500

    add iconButton

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -5,6 +5,7 @@ import { MEALS } from "../data/dummy-data"
 import MealDetails from "../components/MealDetails"
 import Subtitle from "../components/MealDetail/Subtitle"
 import List from "../components/MealDetail/List"
+import IconButton from "../components/IconButton"
 
 function MealDetailScreen({ route, navigation }) {
   const mealId = route.params.mealId
@@ -17,7 +18,13 @@ function MealDetailScreen({ route, navigation }) {
   useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => {
-        return <Button title="Hit Me!" onPress={headerButtonPressHandler} />
+        return (
+          <IconButton 
+            onPress={headerButtonPressHandler} 
+            icon='star'
+            color='white'
+          />
+        )
       }
     })
   }, [navigation, headerButtonPressHandler])
